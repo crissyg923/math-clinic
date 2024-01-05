768eb54fb6cbf1b15f9fc439bf0ba12bf1fd5959
Crystal Green <crissybrown1992@icloud.com>
2024-01-05 13:55:58 -0500

    restart back with initial code to implement openAI

--- client/src/pages/Fractions/Divide/DivideFract.jsx
+++ client/src/pages/Fractions/Divide/DivideFract.jsx
@@ -1,35 +1,40 @@
 import { ReactSVG } from "react-svg";
 import divfrac from '../../../images/dividing-fract.svg'
-import { Image, Box } from '@chakra-ui/react'
+import { useState } from 'react';
 
-import OpenAI from "openai";
-import 'dotenv/config';
 
-const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+// import OpenAI from "openai";
+// import 'dotenv/config';
 
-const openai = new OpenAI({ key: apiKey });
+// const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}],
-    model: "gpt-3.5-turbo",
-  });
+// const openai = new OpenAI({ key: apiKey });
 
-  console.log(completion.choices[0]);
-}
-main();
+// async function main() {
+//   const completion = await openai.chat.completions.create({
+//     messages: [{"role": "system", "content": "You are a helpful assistant."},
+//         {"role": "user", "content": "Who won the world series in 2020?"},
+//         {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+//         {"role": "user", "content": "Where was it played?"}],
+//     model: "gpt-3.5-turbo",
+//   });
+
+//   console.log(completion.choices[0]);
+// }
+// main();
 
 export default function Divide() {
+// const [results, setResults] = useState('');
+// const handleButton = (prompt) => {
+//   const {target}
+// }
     return (
     <div className="container" >
     <div className="displaybox" style={{border: '3px solid green'}}>
 
       <ReactSVG src={divfrac} className="svg-container" />
       </div>
-      <p>{apiKey}</p>
+     <button></button> 
     </div>
     )
 }
