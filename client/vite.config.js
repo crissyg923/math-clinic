1ed815ede3ec0b23c6c696e25e01783321fe087c
Crystal Green <crissybrown1992@icloud.com>
2024-01-28 12:30:19 -0500

    updated vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://mathclinic.netlify.app/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   server: {
     port: 3000,
